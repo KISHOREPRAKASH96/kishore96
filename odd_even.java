32d54db374af026072f083edcc2807515dbe889e
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-06-06 12:08:40 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class odd_even
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println(n);
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
