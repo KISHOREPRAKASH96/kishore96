c4969a812db2bb538ca2c070c25cff623b0638b0
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-06-06 17:07:53 +0530

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class hello_world
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("HELLO WORLD");
+        }
+    }
+}
