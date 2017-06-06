5255e148c05e1dfb9aa328d07ec7318d915b2216
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-06-06 21:42:13 +0530

    Create natural_num.java

---
+++ natural_num.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class natural_num
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+             System.out.println(i);
+        }
+    }
+}
