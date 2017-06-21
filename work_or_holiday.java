c1e9bfdf9352859f602574193169230907b499ed
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-06-21 16:55:10 +0530

    Create work_or_holiday.java

---
+++ work_or_holiday.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+class work_or_holiday
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String t=s.nextLine();
+        switch(t)
+        {
+            case "monday":
+                {
+                    System.out.println("true");
+                    break;
+                }
+                case "tuesday":
+                {
+                    System.out.println("true");
+                    break;
+                }
+                case "wednesday":
+                {
+                    System.out.println("true");
+                    break;
+                }
+                case "thursday":
+                {
+                    System.out.println("true");
+                    break;
+                }
+                case "friday":
+                {
+                    System.out.println("true");
+                    break;
+                }
+                case "saturday":
+                {
+                    System.out.println("true");
+                    break;
+                    
+                }
+                case "sunday":
+                {
+                    System.out.println("false");
+                    break;
+                    
+                }
+                default:
+                {
+                    System.out.println("it is not a day");
+                }
+        }
+    }
+}
+
