b88f3553765eb3e804f1a752a4641120264a448a
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-08-03 13:51:00 +0530

    Create player209.java

---
+++ player209.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class player209
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+    	System.out.println("enter the string");
+   		String s1=in.nextLine();
+   		String s2=in.nextLine();
+   		System.out.println(s1+s2);
+        
+    }
+}
