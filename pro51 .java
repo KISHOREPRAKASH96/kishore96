5ae627784db01107c232397373464853e6d9371a
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-08-04 17:58:25 +0530

    Create pro51 .java

---
+++ pro51 .java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pro51 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+	   
+		System.out.println("enter the no of rows and columns in chocolate:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+			int q=(a*b)-1;
+			System.out.println(q);
+
+	}
+}
