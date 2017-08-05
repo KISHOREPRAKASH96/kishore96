ed8e9b9c8556230fd3c021454f12f4933fda7554
KISHOREPRAKASH96 <kishoreprakash64@gmail.com>
2017-08-05 07:01:27 +0530

    Create beginner84.java

---
+++ beginner84.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str;
+    printf("enter the char");
+    scanf("%c", &str);  
+    
+    printf("ascii value of %c = %d",str,str);
+    return 0;
+}
